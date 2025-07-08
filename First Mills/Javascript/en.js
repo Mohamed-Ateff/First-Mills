6662e59a1efe426f1eb91d14b65a799489656e38
Mohamed Atef <mohamedatteef3@gmail.com>
2025-07-08 16:44:43 +0300

    0

--- First Mills/Javascript/en.js
+++ First Mills/Javascript/en.js
@@ -39,7 +39,7 @@ document.addEventListener("DOMContentLoaded", function () {
       body.setAttribute("dir", "ltr");
     } else {
       body.classList.remove("english");
-      languageToggle.textContent = "English";
+      languageToggle.textContent = "En";
       body.setAttribute("lang", "ar");
       body.setAttribute("dir", "rtl");
     }
